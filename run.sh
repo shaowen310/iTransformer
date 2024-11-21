92756caa1de1bb3eb57664deb49b3331db6e2ada
shaowen <shaowen310@outlook.com>
2024-11-21 19:15:23 +0800

    update scripts

--- run.sh
+++ run.sh
@@ -7,7 +7,14 @@ bash scripts/multivariate_forecasting/ETT/iTransformer_ETTm2.sh
 bash scripts/multivariate_forecasting/Weather/iTransformer.sh
 bash scripts/multivariate_forecasting/Exchange/iTransformer.sh
 
-# bash scripts/zeroshot/ETT/iTransformer_ETTh1_ETTh2.sh
-# bash scripts/zeroshot/ETT/iTransformer_ETTh2_ETTh1.sh
-# bash scripts/zeroshot/ETT/iTransformer_ETTm1_ETTm2.sh
-# bash scripts/zeroshot/ETT/iTransformer_ETTm2_ETTm1.sh
+# Zero-shot
+bash scripts/zeroshot/ETT/iTransformer_ETTh1_ETTh2.sh
+bash scripts/zeroshot/ETT/iTransformer_ETTh2_ETTh1.sh
+bash scripts/zeroshot/ETT/iTransformer_ETTm1_ETTm2.sh
+bash scripts/zeroshot/ETT/iTransformer_ETTm2_ETTm1.sh
+
+# Few-shot
+bash scripts/fewshot/ETT/iTransformer_ETTh1.sh
+bash scripts/fewshot/ETT/iTransformer_ETTh2.sh
+bash scripts/fewshot/ETT/iTransformer_ETTm1.sh
+bash scripts/fewshot/ETT/iTransformer_ETTm2.sh
