5b7945f659514b5284b1bbe3d7ee36b8fd306280
shaowen <shaowen310@outlook.com>
2024-11-26 20:01:15 +0800

    cleanup

--- experiments/exp_long_term_forecasting_partial.py
+++ experiments/exp_long_term_forecasting_partial.py
@@ -338,8 +338,8 @@ class Exp_Long_Term_Forecast_Partial(Exp_Basic):
         f.close()
 
         np.save(folder_path + 'metrics.npy', np.array([mae, mse, rmse, mape, mspe]))
-        np.save(folder_path + 'pred.npy', preds)
-        np.save(folder_path + 'true.npy', trues)
+        # np.save(folder_path + 'pred.npy', preds)
+        # np.save(folder_path + 'true.npy', trues)
 
         return
 
